3e881bf260e72f66ee8be9f2a39763e2716cb81d
Ziwen Zeng <ziz236@ucsd.edu>
2018-02-22 10:38:49 -0800

    lab7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,8 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	// your code herega
+	ga("send", "event", "like", "click");
 }
+
+
